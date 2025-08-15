5482373420050d209041e526e81d0f64d775254c
kostabarilo12 <kostabarilo12@gmail.com>
2025-08-15 09:51:18 +0000

    Init

---
+++ eslint.config.mjs
@@ -0,0 +1,27 @@
+import eslint from "@eslint/js";
+import globals from "globals";
+import tseslint from "typescript-eslint";
+
+export default tseslint.config(
+    {
+        files: ["src/**/*.ts"],
+        ignores: ["node_modules", "dist"],
+        languageOptions: {
+            parserOptions: {
+                projectService: true,
+                tsconfigRootDir: import.meta.dirname
+            },
+            globals: globals.node
+        },
+        rules: {
+            "@typescript-eslint/no-explicit-any": "error",
+            "@typescript-eslint/no-floating-promises": "warn",
+            "@typescript-eslint/no-unsafe-argument": "error",
+            "@typescript-eslint/no-unsafe-return": "error",
+            "@typescript-eslint/no-unused-vars": "error"
+        }
+    },
+    tseslint.configs.recommended,
+    eslint.configs.recommended,
+    tseslint.configs.recommendedTypeChecked
+);
---
+++ src/index.ts
@@ -0,0 +1,3 @@
+export function grinch() {
+    console.log("grinch");
+}
