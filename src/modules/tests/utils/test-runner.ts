6b1cf002ac5602b3244fe5cc16d72540bf38b8f9
TarhunchiKKK <kostabarilo12@gmail.com>
2025-08-16 13:53:12 +0300

    feat: Basic Test Runner

--- src/modules/tests/utils/test-runner.ts
+++ src/modules/tests/utils/test-runner.ts
@@ -1,3 +1,18 @@
+import { BaseTest } from "../classes/base-test";
+import { TestFactory } from "./test-factory";
+
 export class TestRunner {
-    public constructor() {}
+    private childrenTests: BaseTest[] = [];
+
+    public constructor(private title: string) {}
+
+    public createTestFactory() {
+        return new TestFactory(this.childrenTests.push);
+    }
+
+    public async run() {
+        for (const test of this.childrenTests) {
+            await test.run();
+        }
+    }
 }
