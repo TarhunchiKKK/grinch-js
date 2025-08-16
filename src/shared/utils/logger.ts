d28e00411e4dd633c9a5eb7be4b3f448a784a3cb
TarhunchiKKK <kostabarilo12@gmail.com>
2025-08-16 19:39:13 +0300

    feat: ConsoleReporter Class

---
+++ src/shared/utils/logger.ts
@@ -0,0 +1,9 @@
+export class Logger {
+    public static success(message: string) {
+        console.log("\x1b[32m%s\x1b[0m", message);
+    }
+
+    public static failure(message: string) {
+        console.log("\x1b[31m%s\x1b[0m", message);
+    }
+}
