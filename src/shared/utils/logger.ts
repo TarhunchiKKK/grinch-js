673ce715d66b466bab0c66e49f832ebdbb172047
TarhunchiKKK <kostabarilo12@gmail.com>
2025-08-17 12:21:19 +0300

    feat: Add warning Method For Logger Class

--- src/shared/utils/logger.ts
+++ src/shared/utils/logger.ts
@@ -3,6 +3,10 @@ export class Logger {
         console.log("\x1b[32m%s\x1b[0m", message, "\n");
     }
 
+    public static warning(message: string) {
+        console.log("\x1b[33m%s\x1b[0m", message, "\n");
+    }
+
     public static failure(message: string) {
         console.log("\x1b[31m%s\x1b[0m", message, "\n");
     }
