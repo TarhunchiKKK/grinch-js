8f3dba9a1c78585c673eecba77021e391858afc5
TahunchiKKK <kostabarilo12@gmail.com>
2025-08-18 08:46:17 +0000

    test: + BaseAssertion.toMatchZodSchema() + BaseAssertion.toSatisfy()

---
+++ tests/unit/assertions/basic/to-match-zod-schema.spec.ts
@@ -0,0 +1,60 @@
+import { describe, test, expect } from "@jest/globals";
+import { faker } from "@faker-js/faker";
+import { assert } from "../../../../src";
+import { z } from "zod";
+
+describe("BaseAssertion.toMatchZodSchema()", () => {
+    test("With Valid Data", () => {
+        const values = [
+            {
+                value: {
+                    name: faker.person.fullName(),
+                    age: faker.number.int(),
+                    address: {
+                        country: faker.address.country(),
+                        city: faker.address.city()
+                    }
+                },
+                schema: z.object({
+                    name: z.string(),
+                    age: z.int(),
+                    address: z.object({
+                        country: z.string(),
+                        city: z.string()
+                    })
+                })
+            }
+        ];
+
+        for (const { value, schema } of values) {
+            expect(() => assert.basic(value).toMatchZodSchema(schema)).not.toThrow();
+        }
+    });
+
+    test("With Valid Data", () => {
+        const values = [
+            {
+                value: {
+                    name: faker.person.fullName(),
+                    age: faker.number.int(),
+                    address: {
+                        country: faker.address.country(),
+                        street: faker.address.street()
+                    }
+                },
+                schema: z.object({
+                    name: z.string(),
+                    age: z.int(),
+                    address: z.object({
+                        country: z.string(),
+                        city: z.string()
+                    })
+                })
+            }
+        ];
+
+        for (const { value, schema } of values) {
+            expect(() => assert.basic(value).toMatchZodSchema(schema)).toThrow();
+        }
+    });
+});
---
+++ tests/unit/assertions/basic/to-satisfy.spec.ts
@@ -0,0 +1,61 @@
+import { describe, test, expect } from "@jest/globals";
+import { faker } from "@faker-js/faker";
+import { assert } from "../../../../src";
+
+describe("BaseAssertion.toSatisfy()", () => {
+    test("With Valid Data", () => {
+        const values = [
+            {
+                value: faker.number.int({ min: 20 }),
+                condition: (value: number) => value >= 20
+            },
+            {
+                value: faker.string.alphanumeric({ length: 20 }),
+                condition: (value: string) => value.length >= 20
+            },
+            {
+                value: true,
+                condition: (value: boolean) => value
+            },
+            {
+                value: Array.from({ length: 10 })
+                    .fill(null)
+                    .map(() => faker.string.alphanumeric()),
+                condition: (value: string[]) => value.every(item => typeof item === "string")
+            }
+        ];
+
+        for (const { value, condition } of values) {
+            // @ts-ignore
+            expect(() => assert.basic(value).toSatisfy(condition)).not.toThrow();
+        }
+    });
+
+    test("With Valid Data", () => {
+        const values = [
+            {
+                value: faker.number.int({ min: 20 }),
+                condition: (value: number) => value < 0
+            },
+            {
+                value: faker.string.alphanumeric({ length: 20 }),
+                condition: (value: string) => value.length <= 10
+            },
+            {
+                value: true,
+                condition: (value: boolean) => !value
+            },
+            {
+                value: Array.from({ length: 10 })
+                    .fill(null)
+                    .map(() => faker.string.alphanumeric()),
+                condition: (value: string[]) => value.every(item => typeof item !== "string")
+            }
+        ];
+
+        for (const value of values) {
+            // @ts-ignore
+            expect(() => assert.basic(value).toSatisfy(condition)).toThrow();
+        }
+    });
+});
