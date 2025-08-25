9f212cb01b22901818ab777011aecb266e3383f4
TahunchiKKK <kostabarilo12@gmail.com>
2025-08-25 06:55:38 +0000

    feat: mapScenarios() Function

--- src/modules/scenarios/index.ts
+++ src/modules/scenarios/index.ts
@@ -1,3 +1,4 @@
 export { createScenario } from "./utils/create-scenario";
+export { mapScenarios } from "./utils/map-scenarios";
 export { Scenario } from "./classes/scenario";
 export type { AvailableScenarioStates } from "./types/state";
