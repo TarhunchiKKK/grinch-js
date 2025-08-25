8e6ec5c171ed905929b5e452ea0e7de9bce19c45
TahunchiKKK <kostabarilo12@gmail.com>
2025-08-25 06:46:51 +0000

    feat: defineConfig() Function

---
+++ src/modules/config/constants/default-config-options.ts
@@ -0,0 +1,7 @@
+import { ConfigOptions } from "../types/config-options";
+
+export const DEFAULT_CONFIG_OPTIONS: ConfigOptions = {
+    entryPoint: "./tests/main.ts",
+    reporter: "console",
+    resultsDirectory: "test-results"
+};
