9b88ec35ebd749156c418b9ba02ddc14a3c933fe
TahunchiKKK <kostabarilo12@gmail.com>
2025-09-04 14:18:54 +0000

    fix: shared

--- src/shared/types/core.ts
+++ src/shared/types/core.ts
@@ -1,5 +1,3 @@
 export type TypeofResponse = "string" | "number" | "boolean" | "bigint" | "object";
 
-export type ClassConstructor = new (...args: unknown[]) => object;
 
-export type ElementType<T> = T extends Array<infer U> ? U : never;
---
+++ src/shared/types/objects.ts
@@ -0,0 +1,1 @@
+export type ClassConstructor = new (...args: unknown[]) => object;
