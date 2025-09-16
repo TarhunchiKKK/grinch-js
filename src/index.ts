3f1b99eb18cd8dcb66169c3bd1a143b4e509ca6c
TarhunchiKKK <kostabarilo12@gmail.com>
2025-09-16 11:49:20 +0300

    style: formatting

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 export { createScenario as scenario, mapScenarios } from "@modules/scenarios";
 export { createReusableTest as reusableTest, reuseTest } from "@modules/reusable-tests";
-export {type Results} from "@modules/testing-tree"
+export { type Results } from "@modules/testing-tree";
 export { assert } from "@modules/assertions";
 
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
