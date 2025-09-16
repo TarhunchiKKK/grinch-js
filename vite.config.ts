b35b3dcce92eb06a62c95cf31275b4727bddb1d0
TarhunchiKKK <kostabarilo12@gmail.com>
2025-09-16 20:21:45 +0300

    style: format configuration files
    Files formatted:
    * tsconfig.json
    * vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,25 +3,25 @@ import { defineConfig } from "vite";
 import dts from "vite-plugin-dts";
 
 export default defineConfig({
-  build: {
-    lib: {
-      entry: resolve(__dirname, "src/index.ts"),
-      name: "Grinch",
-      fileName: "grinch",
+    build: {
+        lib: {
+            entry: resolve(__dirname, "src/index.ts"),
+            name: "Grinch",
+            fileName: "grinch"
+        },
+        rollupOptions: {
+            external: ["commander"]
+        }
     },
-    rollupOptions: {
-      external: ["commander"],
-    },
-  },
-  plugins: [
-    dts({
-      insertTypesEntry: true,
-    }),
-  ],
-  resolve: {
-    alias: {
-      "@modules": resolve(__dirname, "src/modules"),
-      "@shared": resolve(__dirname, "src/shared")
-    },
-  },
+    plugins: [
+        dts({
+            insertTypesEntry: true
+        })
+    ],
+    resolve: {
+        alias: {
+            "@modules": resolve(__dirname, "src/modules"),
+            "@shared": resolve(__dirname, "src/shared")
+        }
+    }
 });
