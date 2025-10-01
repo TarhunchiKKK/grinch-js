2563ad1c0faad09c013a8d18de4a1cd7bf99e1e2
kostabarilo12 <kostabarilo12@gmail.com>
2025-10-01 09:00:39 +0000

    feat: create "ScheduledListenersFactory" class

--- src/modules/listeners/classes/scheduled-listener.ts
+++ src/modules/listeners/classes/scheduled-listener.ts
@@ -1,6 +1,8 @@
-import { TestAborter } from "@modules/test-aborting";
+import { TestAborter, abort } from "@modules/test-aborting";
 import { BaseListener } from "./base-listener";
 import { TestStatus } from "@shared/types";
+import { ListenersStore } from "../model/listeners-array";
+import { ListenerCallback } from "../types";
 
 export class ScheduledListener extends BaseListener {
     public start() {
@@ -17,5 +19,21 @@ export class ScheduledListener extends BaseListener {
         } catch (error) {
             this.status = TestAborter.handleError(error);
         }
+    }
+}
+export class ScheduledListenersFactory<Params> {
+    public constructor(private readonly callback: ListenerCallback<Params>) {}
+
+    public apply(title: string, delay: number, params?: Params) {
+        const id = ListenersStore.id;
+
+        const listener = new ScheduledListener(id, title, delay, () => this.callback({ params, abort }));
+
+        ListenersStore.add(listener);
+
+        return () => {
+            ListenersStore.remove(id);
+            listener.cancel();
+        };
     }
 }
